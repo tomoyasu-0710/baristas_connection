b868a71edd4804e33d58792c11b77ed4633fe844
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-05-23 14:34:25 +0000

    カッピングノート投稿フォーム作成

--- app/controllers/public/cupping_notes_controller.rb
+++ app/controllers/public/cupping_notes_controller.rb
@@ -1,6 +1,7 @@
 class Public::CuppingNotesController < ApplicationController
   
   def new
+    
   end
 
   def index
@@ -20,4 +21,20 @@ class Public::CuppingNotesController < ApplicationController
 
   def destroy
   end
+  
+  def calculate_total
+    flavor = params[:flavor].to_i
+    after_taste = params[:after_taste].to_i
+    acidity = params[:acidity].to_i
+    mouth_feel = params[:mouth_feel].to_i
+    clean_cup = params[:clean_cup].to_i
+    sweetness = params[:sweetness].to_i
+    harmony = params[:harmony].to_i
+  
+    @total = flavor + after_taste + acidity + mouth_feel + clean_cup + sweetness + harmony
+  
+    # 合計値をビューテンプレートへ渡すためにインスタンス変数に代入
+    @total = @total
+  end
+  
 end
