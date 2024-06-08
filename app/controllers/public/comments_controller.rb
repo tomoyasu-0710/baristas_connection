8a120391128a7f820c8253c3cf62086e550ce8a0
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-06-08 14:23:40 +0000

    コメント機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,4 +1,7 @@
 class Public::CommentsController < ApplicationController
+  
+  before_action :set_commentable, only: [:show, :create, :destroy]
+  
   def index
   end
 
@@ -6,8 +9,34 @@ class Public::CommentsController < ApplicationController
   end
 
   def create
+    @comment = @commentable.comments.build(comment_params)
+    if @comment.save
+      redirect_to @commentable, notice: "コメントしました。"
+    else
+      redirect_to @commentable, alert: "コメントに失敗しました。"
+    end
   end
 
   def destroy
+     @comment = @commentable.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @commentable, notice: "コメントを削除しました。"
   end
+  
+   private
+
+  def set_commentable
+    if params[:post_id]
+      @commentable = Post.find(params[:post_id])
+    elsif params[:group_id]
+      @commentable = Group.find(params[:group_id])
+    elsif params[:cupping_note_id]
+      @commentable = CuppingNote.find(params[:cupping_note_id])
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+  
 end
